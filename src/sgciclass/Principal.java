ea5ef2d0861c3c312553f8ccaca7809daa376d09
Davilucasanteves <24680000za@gmail.com>
2025-11-28 08:36:12 -0300

    patch: fxmlLoader

--- src/sgciclass/Principal.java
+++ src/sgciclass/Principal.java
@@ -1,6 +1,36 @@
 package sgciclass;
 
-public class Principal {
-    public static void main(String [] args)
-    {System.out.println("OLA MUNDO!!!!!");}
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import util.CaminhoArquivo;
+
+public class Principal extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(CaminhoArquivo.TELA_LOGIN));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            Image imagemIconeDaJanela = new Image("icons/icon_logo.png");
+            stage.getIcons().add(imagemIconeDaJanela);// define o icone da janela
+            stage.setTitle("Acadêmico");
+            stage.setResizable(false);
+
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
