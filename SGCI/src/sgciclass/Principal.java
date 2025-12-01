6b178c594cd0b0ed52b24bc9a4dce8177f9ee04f
Lívia Maria <liviamariaprates236@gmail.com>
2025-12-01 08:42:53 -0300

    salvando alterações

--- SGCI/src/sgciclass/Principal.java
+++ SGCI/src/sgciclass/Principal.java
@@ -13,7 +13,7 @@ public class Principal extends Application {
     @Override
     public void start(Stage stage) {
             try{
-            Parent root = FXMLLoader.load(getClass().getResource("/View/TelaLogin.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/View/TelaCadastroCliente.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();}
