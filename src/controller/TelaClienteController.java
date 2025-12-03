38f05128e8e6fa082bfe6551a83ff577800ca39c
Davilucasanteves <24680000za@gmail.com>
2025-12-03 00:17:02 -0300

    Todos os fxml estão para java-21 e adiciono botão sair de cliente

--- src/controller/TelaClienteController.java
+++ src/controller/TelaClienteController.java
@@ -2,16 +2,28 @@
 package controller;
 
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class TelaClienteController {
 
     @FXML
     private ImageView imageDeletarFesta;
+
+    @FXML
+    private Button buttomSair;
 
     @FXML
     private ImageView imageInformacoesIngressos;
@@ -26,13 +38,27 @@ public class TelaClienteController {
     private TableColumn<?, ?> tableColunmnNomeFesta;
 
     @FXML
-    void handleCriarFesta(MouseEvent event) {
-
-    }
+    private TableView<?> tableFesta;
 
     @FXML
-    void handleVerIngressosAVenda(KeyEvent event) {
+    public void initialize() {
+        // Inicialização da tela de cliente
+    }
 
+
+    @FXML
+    void handleVerIngressosAVenda(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/TelaIngressosDisponiveis.fxml"));
+        AnchorPane page = loader.load();
+        
+        Stage stage = new Stage();
+        stage.setTitle("Ingressos Disponíveis");
+        stage.setScene(new Scene(page));
+        stage.show();
+        
+        // Fecha a tela de cliente
+        Stage atual = (Stage) imageVerIngressosAVenda.getScene().getWindow();
+        atual.close();
     }
 
     @FXML
@@ -41,8 +67,23 @@ public class TelaClienteController {
     }
 
     @FXML
-    void handleimageInformacoesIngressos(KeyEvent event) {
+    void handleimageInformacoesIngressos(MouseEvent event) {
 
+    }
+
+    @FXML
+    void handleSair(ActionEvent event) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/TelaInicialSGVI.fxml"));
+        Parent root = loader.load();
+        
+        Stage stage = new Stage();
+        stage.setTitle("Sistema de Ingressos");
+        stage.setScene(new Scene(root));
+        stage.show();
+        
+        // Fecha a tela de administrador
+        Stage atual = (Stage) buttomSair.getScene().getWindow();
+        atual.close();
     }
 
 }
